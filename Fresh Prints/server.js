6e3f532d858b5580b4975b64e0fff5b8a0f08b7c
Akshay Chandrasekhar <aksch@seas.upenn.edu>
2014-09-21 02:05:34 -0400

    JSON

--- Fresh Prints/server.js
+++ Fresh Prints/server.js
@@ -1,13 +1,20 @@
-
 	var MobileServiceClient = WindowsAzure.MobileServiceClient;
 	var client = new MobileServiceClient('https://freshprintsservice.azure-mobile.net/', 'baJFydlmYLROPHHmpckTpnXbxKNnIK59');
 	var todoItemTable = client.getTable('todoitem');
 	
-	var query = todoItemTable.where({
-		complete: false
-	}).read().done(function (results) {
-		alert(JSON.stringify(results));
+	var query = todoItemTable.read().done(function (results) {
+		if(results[0].text == 'waiting') {
+			todoItemTable.update({
+				id: results[0].id,
+				text: 'verifytime'
+			})
+		}	
+		if(results[0].text == 'verifysuccess') {
+			todoItemTable.update({
+				id: results[0].id,
+				text: 'waiting'
+			})
+		}
 	}, function (err) {
 		alert("Error: " + err);
-	});
-	console.log(query);
+	})
