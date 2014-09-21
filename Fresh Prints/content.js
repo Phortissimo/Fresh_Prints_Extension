68341f8c19f13c8c6d8ec5611990d844d698487c
Akshay Chandrasekhar <aksch@seas.upenn.edu>
2014-09-20 20:05:16 -0400

    Spacing of stuff

--- Fresh Prints/content.js
+++ Fresh Prints/content.js
@@ -5,7 +5,7 @@
 
     document.getElementsByTagName('head')[0].appendChild(script)
 	var passfield = document.getElementById("Passwd");
-	passfield.setAttribute('style','visibility:hidden');
+	passfield.setAttribute('style','display:none');
 	var staySignedIn = document.getElementById("PersistentCookie");
 	staySignedIn.disabled = true;
 	staySignedIn.checked = false;
@@ -14,6 +14,12 @@
 	var errormsg = document.getElementById("errormsg_0_Email");
 	errormsg.setAttribute('style','visibility:hidden');
 	
+	var printImage = document.createElement("img");
+	printImage.src = "http://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Music-fortissimo.png/180px-Music-fortissimo.png";
+	printImage.setAttribute('alt','Waiting for fingerprint');
+	$("#Email").after(printImage);
+	printImage.setAttribute('style','display:none');
+	
 	var signIn = document.getElementById("signIn");
 	signIn.value = "Sign in with Fresh Prints!";
 	signIn.onclick = function(e) {
@@ -21,11 +27,8 @@
 		if(!click) {
 			//signIn.value = "Verifying fingerprint...";
 			signIn.setAttribute('style','visibility:hidden');
+			printImage.setAttribute('style','display:inline');
 			click = true;
-			var printImage = document.createElement("img");
-			printImage.src = "http://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Music-fortissimo.png/180px-Music-fortissimo.png";
-			printImage.setAttribute('alt','Waiting for fingerprint');;
-			$("#Email").after(printImage);
 		}else {
 			click = false;
 			signIn.setAttribute('style','visibility:visible');
